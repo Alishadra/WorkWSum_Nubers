c164acc3b54cc2d40835a8c156f3ac322287db41
HOME <HOME@DESKTOP-3QBHBVK>
2019-02-24 15:47:40 +0200

    SumNumbs

---
+++ WorkWihtSumNumbers/src/org/sumNumbers/SumNumbers.java
@@ -0,0 +1,50 @@
+package org.sumNumbers;
+
+import java.util.logging.Logger;
+
+
+public class SumNumbers {
+
+	private static Logger logger = Logger.getLogger(SumNumbers.class.getName());
+	
+	public static void main(String[] args) {
+
+
+		int outNum = 12345;
+		
+		logger.info(String.valueOf(getSumNumbersByDivision(outNum)));
+		logger.info(String.valueOf(getSumNumbersByChars(outNum)));
+	
+	}
+	private static int getSumNumbersByChars(int outNum) {
+
+		String numb = String.valueOf(outNum);
+		
+		char[] numbers = numb.toCharArray();
+		
+		int result=0;
+		
+		for(char number : numbers) {
+			
+			int newNum = Character.getNumericValue(number);
+			
+			result += newNum;
+		}
+		
+		return result;
+	}
+	
+	private static int getSumNumbersByDivision(int outNum) {
+
+		int sum = 0;
+		
+		for(; outNum > 0; outNum = outNum/10) {
+			
+			sum += outNum % 10;
+		}
+		
+		return sum;
+	}
+	
+
+}
